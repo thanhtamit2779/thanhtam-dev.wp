a00ad5fcc539ed8c1e77ad965a4b6e248a9e9a86
DESKTOP-FHPRAEF\thanh <thanhtam.it2779@gmail.com>
2017-09-18 09:47:29 +0700

    [UPDATE] - SOURCE CODE WORDPRESS
    - Version Wordpress 4.8.1
    - Theme Newspaper Webdoctor.vn

---
+++ wp-content/themes/hugeshop/page-templates/thong-tin-thanh-vien.php
@@ -0,0 +1,44 @@
+<?php
+/**
+ * Template Name: USER INFO LIST
+ *
+ * Description: Hiển thị thông tin danh sách thành viên
+ *
+ * @package WordPress
+ * @subpackage hugeshop_Themes
+ * @since Huge Shop 1.0
+ */
+global $hugeshop_opt;
+
+get_header();
+?>
+<div class="main-container about-page">
+	<div class="title-breadcrumb">
+		<div class="container">
+			<div class="title-breadcrumb-inner">
+				<header class="entry-header">
+					<h1 class="entry-title"><?php the_title(); ?></h1>
+				</header>
+				<?php HugeShop::hugeshop_breadcrumb(); ?>
+			</div>
+		</div>
+	</div>
+	<div class="page-content">
+		<div class="about-container">
+			<?php while ( have_posts() ) : the_post(); ?>
+				<?php get_template_part( 'content', 'page' ); ?>
+			<?php endwhile; ?>
+		</div>
+		<div class="brands-logo other-page">
+			<div class="container">
+			<?php echo do_shortcode('[ourbrands]'); ?>
+			</div>
+		</div>
+		<div class="home-static3 other-page">
+			<div class="container">
+				
+			</div>	
+		</div>
+	</div>
+</div>
+<?php get_footer(); ?>
